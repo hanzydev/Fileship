7667e125a68df5cbd6fd7430df8c5268a1ade1c0
hanzydev <contact@hanzy.dev>
2024-07-17 13:38:39 +0300

    feat: type filtering

--- app/pages/dashboard/files/index.vue
+++ app/pages/dashboard/files/index.vue
@@ -6,7 +6,69 @@
 
         <div space-y-6>
             <h2>Files</h2>
-            <UiSearchBar v-model="searchQuery" placeholder="Search files..." />
+            <div flex="~ gap4 1 items-center" wfull>
+                <UiSearchBar
+                    v-model="searchQuery"
+                    placeholder="Search files..."
+                    wfull
+                />
+                <UiDropdown placement="bottom">
+                    <UiButton
+                        icon="heroicons-solid:filter"
+                        icon-size="20"
+                        h12
+                        w56
+                        gap2
+                        text-slate300
+                    >
+                        Filter type
+                    </UiButton>
+                    <template #content>
+                        <div
+                            max-h64
+                            w56
+                            overflow-y-auto
+                            rounded-lg
+                            bg-fs3
+                            p1.5
+                            space-y-1
+                            ring="1 fs-accent"
+                        >
+                            <UiButton
+                                v-for="_type in ['image', 'video', 'audio']"
+                                :key="_type"
+                                :icon="
+                                    filterType.includes(_type as never)
+                                        ? 'heroicons-solid:check'
+                                        : 'heroicons-solid:filter'
+                                "
+                                :variant="
+                                    filterType.includes(_type as never)
+                                        ? 'accent'
+                                        : 'primary'
+                                "
+                                icon-size="20"
+                                wfull
+                                gap2
+                                @click="
+                                    () => {
+                                        const findIndex = filterType.indexOf(
+                                            _type as never,
+                                        );
+                                        if (findIndex === -1) {
+                                            filterType.push(_type as never);
+                                        } else {
+                                            filterType.splice(findIndex, 1);
+                                        }
+                                    }
+                                "
+                            >
+                                {{ titleCase(_type) }}
+                            </UiButton>
+                        </div>
+                    </template>
+                </UiDropdown>
+            </div>
             <div grid="~ gap6 lg:cols-3 md:cols-2 xl:cols-4">
                 <New h208px @action="$router.push('/dashboard/files/upload')" />
                 <FileCard
@@ -25,11 +87,14 @@
 </template>
 
 <script setup lang="ts">
+import { titleCase } from 'scule';
+
 const files = useFiles();
 const folders = useFolders();
 
 const searchQuery = ref('');
 const currentPage = ref(1);
+const filterType = ref([]);
 
 const { data: foldersData } = await useFetch('/api/folders');
 const { data: filesData } = await useFetch('/api/files');
@@ -48,6 +113,11 @@ files.value = filesData.value!.map((f) => ({
 const filtered = computed(() =>
     files.value
         .filter((f) => !f.folderId)
+        .filter(
+            (f) =>
+                !filterType.value.length ||
+                filterType.value.some((t) => f.mimeType.startsWith(`${t}/`)),
+        )
         .filter((f) =>
             Object.values(f).some((v) =>
                 v
