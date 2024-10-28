3d156f977d95f804ce504858ef712bd0324d8f20
hanzydev <contact@hanzy.dev>
2024-10-28 17:05:28 +0300

    refactor: remove caching for version info

--- server/api/version.get.ts
+++ server/api/version.get.ts
@@ -2,25 +2,22 @@ import pkg from '~~/package.json';
 
 const repoUrl = 'hanzydev/Fileship';
 
-export default cachedEventHandler(
-    async () => {
-        const response = {
-            number: pkg.version,
-            url: `https://github.com/${repoUrl}/releases/tag/v${pkg.version}`,
+export default defineEventHandler(async () => {
+    const response = {
+        number: pkg.version,
+        url: `https://github.com/${repoUrl}/releases/tag/v${pkg.version}`,
+    };
+
+    try {
+        const latestRelease = await $fetch<{ tag_name: string }>(
+            `https://api.github.com/repos/${repoUrl}/releases/latest`,
+        );
+
+        return {
+            ...response,
+            latest: pkg.version === latestRelease.tag_name.slice(1),
         };
-
-        try {
-            const latestRelease = await $fetch<{ tag_name: string }>(
-                `https://api.github.com/repos/${repoUrl}/releases/latest`,
-            );
-
-            return {
-                ...response,
-                latest: pkg.version === latestRelease.tag_name.slice(1),
-            };
-        } catch {
-            return { ...response, latest: true };
-        }
-    },
-    { maxAge: 1_800 },
-);
+    } catch {
+        return { ...response, latest: true };
+    }
+});
