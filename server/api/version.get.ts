afb6c108f6e80afb1f80b0ecf34c8f5036c5811e
hanzydev <contact@hanzy.dev>
2024-10-28 19:49:41 +0300

    refactor: better cacheing function for version information

--- server/api/version.get.ts
+++ server/api/version.get.ts
@@ -2,22 +2,24 @@ import pkg from '~~/package.json';
 
 const repoUrl = 'hanzydev/Fileship';
 
+const cachedIsLatest = defineCachedFunction(
+    async () => {
+        try {
+            const release = await $fetch<{ tag_name: string }>(
+                `https://api.github.com/repos/${repoUrl}/releases/latest`,
+            );
+            return pkg.version === release.tag_name.slice(1);
+        } catch {
+            return false;
+        }
+    },
+    { maxAge: 1_800 },
+);
+
 export default defineEventHandler(async () => {
-    const response = {
+    return {
         number: pkg.version,
         url: `https://github.com/${repoUrl}/releases/tag/v${pkg.version}`,
+        latest: await cachedIsLatest(),
     };
-
-    try {
-        const latestRelease = await $fetch<{ tag_name: string }>(
-            `https://api.github.com/repos/${repoUrl}/releases/latest`,
-        );
-
-        return {
-            ...response,
-            latest: pkg.version === latestRelease.tag_name.slice(1),
-        };
-    } catch {
-        return { ...response, latest: true };
-    }
 });
