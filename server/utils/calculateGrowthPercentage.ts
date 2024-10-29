8e5913387107e92c993f2150c0cde8a1282a48af
hanzydev <contact@hanzy.dev>
2024-10-29 18:15:06 +0300

    refactor: remove unnecessary if statement

--- server/utils/calculateGrowthPercentage.ts
+++ server/utils/calculateGrowthPercentage.ts
@@ -2,9 +2,6 @@ export const calculateGrowthPercentage = (
     current: number | bigint,
     previous: number | bigint,
 ) => {
-    if (current > Number.MAX_SAFE_INTEGER) current = Number.MAX_SAFE_INTEGER;
-    if (previous > Number.MAX_SAFE_INTEGER) previous = Number.MAX_SAFE_INTEGER;
-
     current = Number(current);
     previous = Number(previous);
 
