69a793776b415e3f2422eb6af354477d480d33e6
hanzydev <contact@hanzy.dev>
2025-06-29 19:18:12 +0300

    refactor(prisma)!: use migrations

--- server/plugins/runTasks.ts
+++ server/plugins/runTasks.ts
@@ -1,6 +1,8 @@
 export default defineNitroPlugin(async () => {
+    await runTask('db:migrate');
     await runTask('db:createDefaultUser');
     await runTask('db:resetBackupRestoreState');
+
     await runTask('dir:clearTemp');
     await runTask('dir:createDirectories');
 });
