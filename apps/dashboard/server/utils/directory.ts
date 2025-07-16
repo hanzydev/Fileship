9d3d3809d1d1175ecb4bf92dc4a89de1d9cb0eea
hanzydev <contact@hanzy.dev>
2025-07-16 04:39:42 +0300

    refactor: run server file directly instead of running preview command

--- apps/dashboard/server/utils/directory.ts
+++ apps/dashboard/server/utils/directory.ts
@@ -1,6 +1,6 @@
 import { join } from 'pathe';
 
-export const baseDirectory = join(process.env.NODE_ENV === 'development' ? './' : '../');
+export const baseDirectory = join(process.env.NODE_ENV === 'development' ? './' : process.cwd());
 
 export const rootDirectory = join(baseDirectory, '..', '..');
 
