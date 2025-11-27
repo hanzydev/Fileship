d5ad5b991ed1e482e8744ee9e95a180beadd8e83
hanzydev <contact@hanzy.dev>
2025-11-27 22:11:59 +0300

    refactor: update background color opacity for Sidebar and Modal components

--- apps/dashboard/app/components/ui/Modal.vue
+++ apps/dashboard/app/components/ui/Modal.vue
@@ -8,7 +8,7 @@
             <div
                 v-if="isOpen"
                 text-left="!"
-                bg="black/20"
+                bg="black/40"
                 fixed
                 inset-0
                 z40
