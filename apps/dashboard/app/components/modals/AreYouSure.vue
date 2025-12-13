41bc4153036ac22b1c768cb9fae20bfc1450f874
hanzydev <contact@hanzy.dev>
2025-12-13 22:50:43 +0300

    style: improve layout and spacing in AreYouSure modal for better readability

--- apps/dashboard/app/components/modals/AreYouSure.vue
+++ apps/dashboard/app/components/modals/AreYouSure.vue
@@ -1,11 +1,16 @@
 <template>
     <UiModal v-model="isOpen">
-        <div flex="~ col gap4 justify-center items-center" wfull p8 text-center>
-            <Icon name="heroicons:exclamation-triangle-solid" size="96" />
-            <h3>{{ title }}</h3>
-            <p text-fs-muted-2 font-medium>
-                {{ description }}
-            </p>
+        <div flex="~ col justify-center gap8" wfull p8>
+            <div flex="~ lt-sm:(col justify-center) items-center gap4" lt-sm="text-center">
+                <Icon name="heroicons:exclamation-triangle-solid" size="96" />
+                <div lt-sm:space-y-2>
+                    <h3>{{ title }}</h3>
+                    <p text-fs-muted-1>
+                        {{ description }}
+                    </p>
+                </div>
+            </div>
+            <slot name="extra" />
             <div grid="~ gap2 sm:cols-2 sm:gap4" wfull>
                 <UiButton
                     alignment="center"
