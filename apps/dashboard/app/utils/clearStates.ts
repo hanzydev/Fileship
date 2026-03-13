82356d379453bdbc8ad9a415b8f0a294ec36461e
hanzydev <contact@hanzy.dev>
2026-03-14 02:22:55 +0300

    refactor: simplify clearStates function by using clearNuxtState composable

--- apps/dashboard/app/utils/clearStates.ts
+++ apps/dashboard/app/utils/clearStates.ts
@@ -1,41 +1,29 @@
 export const clearStates = (hard = false, clearUserAndTheme = false) => {
-    const logs = useLogs();
-    const notes = useNotes();
-    const files = useFiles();
-    const users = useUsers();
-    const stats = useStats();
-    const embed = useEmbed();
-    const domains = useDomains();
-    const folders = useFolders();
-    const backups = useBackups();
-    const sessions = useSessions();
-    const passkeys = usePasskeys();
     const currentUser = useAuthUser();
-    const currentTheme = useTheme();
 
-    notes.value = [];
-    files.value = [];
-    folders.value = [];
-    stats.value = null;
-
-    currentUser.value!.stats = {
-        notes: 0,
-        files: 0,
-        folders: 0,
-    };
+    clearNuxtState('notes');
+    clearNuxtState('files');
+    clearNuxtState('folders');
+    clearNuxtState('stats');
 
     if (hard) {
-        users.value = [];
-        domains.value = [];
-        backups.value = [];
-        sessions.value = [];
-        passkeys.value = [];
-        logs.value = { users: [], logs: [] };
-        embed.value = defaultEmbed;
+        clearNuxtState('users');
+        clearNuxtState('domains');
+        clearNuxtState('backups');
+        clearNuxtState('sessions');
+        clearNuxtState('passkeys');
+        clearNuxtState('logs');
+        clearNuxtState('embed');
     }
 
     if (clearUserAndTheme) {
-        currentUser.value = null;
-        currentTheme.value = 'Fileship';
+        clearNuxtState('authUser');
+        clearNuxtState('theme');
+    } else {
+        currentUser.value!.stats = {
+            notes: 0,
+            files: 0,
+            folders: 0,
+        };
     }
 };
