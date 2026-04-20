e8265302de2b5750cb8ed804809a6b61c6a0a898
hanzydev <contact@hanzy.dev>
2026-04-21 02:03:07 +0300

    feat(notes): add preview toggle, and copy functionality to public page

--- apps/dashboard/app/pages/(view)/note/[id].vue
+++ apps/dashboard/app/pages/(view)/note/[id].vue
@@ -9,7 +9,40 @@
             </template>
         </Head>
 
-        <MarkdownRenderer :content="data!.content" />
+        <MarkdownRenderer v-if="previewMode" :content="data!.content" />
+        <div v-else p8>
+            <pre max-w-full break-all text-wrap w="[calc(100%-140px)]">{{ data!.content }}</pre>
+        </div>
+
+        <div
+            flex="~ items-center justify-center gap-1"
+            ring="1 fs-overlay-4"
+            absolute
+            right-8
+            top-8
+            rounded-2xl
+            bg-fs-overlay-2
+            p1
+        >
+            <UiButton
+                alignment="center"
+                variant="onOverlay"
+                class="size-9 shrink-0 text-fs-muted-2 !rounded-xl !p0 hover:text-white"
+                :icon="previewMode ? 'solar:text-bold' : 'solar:eye-bold'"
+                icon-size="20"
+                aria-label="previewMode ? 'Preview' : 'Raw'"
+                @click="previewMode = !previewMode"
+            />
+            <UiButton
+                alignment="center"
+                variant="onOverlay"
+                class="size-9 shrink-0 text-fs-muted-2 !rounded-xl !p0 hover:text-white"
+                :icon="copied ? 'solar:clipboard-check-bold' : 'solar:clipboard-bold'"
+                :icon-class="copied && 'text-green500!'"
+                icon-size="20"
+                @click="handleCopy"
+            />
+        </div>
     </div>
 </template>
 
@@ -19,6 +52,10 @@ const route = useRoute();
 const { data, error } = await useFetch<NoteData & { embed: Pick<IEmbed, 'enabled' | 'color'> }>(
     `/api/notes/${route.params.id}`,
 );
+
+const previewMode = ref(true);
+const { $toast } = useNuxtApp();
+const { copied, copy } = useClipboard({ legacy: true });
 
 if (error.value?.statusCode === 404) {
     throw createError({
@@ -31,4 +68,9 @@ if (error.value?.statusCode === 404) {
         message: 'You do not have permission to access this page',
     });
 }
+
+const handleCopy = () => {
+    copy(data.value!.content);
+    $toast.success('Note content copied to clipboard');
+};
 </script>
